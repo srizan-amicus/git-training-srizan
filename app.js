3d0f99e9972527370475ea7b0da572a474766868
srizan-amicus <srizan_singh@amicusglobal.com>
2026-07-09 18:57:19 +0530

    feat: added sidebar script

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Hello, World!, This is a sample for Git training");
 console.log("Navigation Section Added");
+
+console.log("Sidebar added")
+const sidebar = document.querySelector("aside");
+console.log(sidebar);
